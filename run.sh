545d0e4d1fe3fae74c64cbd71eb1687fc498b38a
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2021-06-12 18:59:19 +0300

    Default timeout to 1 second

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export SPOTIPY_CLIENT_ID=""
 export SPOTIPY_CLIENT_SECRET=""
 export SPOTIPY_REDIRECT_URI="http://localhost:9999"
 
-python3 ./run.py "<user_id>" "<your-playlist's-name>" "<timeout_insecond_int>"
+python3 ./run.py "<user_id>" "<your-playlist's-name>" 1
