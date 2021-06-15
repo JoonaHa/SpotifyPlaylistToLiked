8a0b9d2fb8137f733dc76f2592ad71597083fa20
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2021-06-15 14:19:54 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,8 +3,8 @@ SET SPOTIPY_CLIENT_ID=""
 SET SPOTIPY_CLIENT_SECRET=""
 SET SPOTIPY_REDIRECT_URI="http://localhost:9999"
 
-python run.py "<user_id>" "<your-playlist's-name>" 1
+python3 run.py "<user_id>" "<your-playlist's-name>" 1
 
-ECHO You can close the window now 
+ECHO Ready. Press any key to close the program
 
 PAUSE>NUL
